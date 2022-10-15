de19a798bb338520ef8265808efc17a207f8b25f
AkashNarale <akashnarale43@gmail.com>
2022-10-15 17:35:16 +0530

    UC1

---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day6_LogicalPrograms
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Day 6 Logical Programming");
+            Console.WriteLine("Program #1: Fibonacci Series");
+            Console.WriteLine("Program #2: Perfect Numbers");
+            Console.WriteLine("Program #3: Prime Numbers");
+            Console.WriteLine("Program #4: Reverse A Number");
+            Console.WriteLine("Program #5: Coupon Numbers");
+            Console.WriteLine("Program #6: Stopwatch Simulator");
+
+            Console.Write("\nPlease select a program to run from options above: ");
+            int choice = Convert.ToInt32(Console.ReadLine());
+
+            switch (choice)
+            {
+                case 1:
+                    fibonacci.UC1();
+                    break;
+                case 2:
+                    perfectNumber.UC2();
+                    break;
+                case 3:
+                    primeNumber.UC3();
+                    break;
+                case 4:
+                    reverseNumber.UC4();
+                    break;
+                case 5:
+                    couponNumber.UC5();
+                    break;
+                case 6:
+                    stopWatch.UC6();
+                    break;
+                default:
+                    Console.WriteLine("Please enter a valid number from given options");
+                    break;
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ fibonacci.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day6_LogicalPrograms
+{
+    internal class fibonacci
+    {
+        //program for fibonacci
+        public static void UC1()
+        {
+            int res;
+            //taking input from user
+            Console.WriteLine("Enter two numbers :");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            int num2 = Convert.ToInt32(Console.ReadLine());
+            //int num1=0;
+            //int num2=1;
+
+            //asking till what count we want
+            Console.WriteLine("Enter tthe count :");
+            int count = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 1; i <= count; i++)
+            {
+                res = num1 + num2;
+                Console.WriteLine(res + " ");
+                num1 = num2;
+                num2 = res;
+            }
+        }
+    }
+}
